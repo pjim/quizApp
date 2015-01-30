a2bcf4fdae9819bb38f0f9ff692708232606ed19
paul james <paul.k.james@googlemail.com>
2015-01-30 11:13:46 +0000

    added an if/else that prints the score when the questions run out

--- app.js
+++ app.js
@@ -28,7 +28,9 @@ function displayQuestion(){
              
              if(correctRadio.checked === true){
               userScore++;
+            
              } 
+            console.log(userScore);
      }
     
      
@@ -50,22 +52,33 @@ function displayQuestion(){
     var displayForm = document.createElement("form");
         formContainer.appendChild(displayForm);
     var answerGroup;
- 
-    for( var i = 0; i < questions[questionNumber].answers.length; i++){
-      answerGroup = document.createElement("p");
-      
-      var radio = document.createElement("input");
-      
-      radio.type ="radio";
-      radio.name = "answers"; 
-      radio.id = questions[questionNumber].answers[i];
-      var answerText =  document.createTextNode(questions[questionNumber].answers[i]);
-       
-      answerGroup.appendChild(answerText);
-        
-      
-      displayForm.appendChild(answerGroup);
-      displayForm.appendChild(radio);
+    
+    if(questions[questionNumber] === 'undefined'){
+        var scoreDisplay = document.createElement('h2');
+        var scoreText = 'Your score is ' + userScore;
+        var scoreNode = document.createTextNode(scoreText);          
+
+
+        scoreDisplay.appendChild(scoreNode);
+        formContainer.appendChild(scoreDisplay);
+    }else{ 
+     
+           for( var i = 0; i < questions[questionNumber].answers.length; i++){
+          answerGroup = document.createElement("p");
+          
+          var radio = document.createElement("input");
+          
+          radio.type ="radio";
+          radio.name = "answers"; 
+          radio.id = questions[questionNumber].answers[i];
+          var answerText =  document.createTextNode(questions[questionNumber].answers[i]);
+           
+          answerGroup.appendChild(answerText);
+            
+          
+          displayForm.appendChild(answerGroup);
+          displayForm.appendChild(radio);
+          }
     }
     
      questionNumber++;         
